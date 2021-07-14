9fab424d40a47e51c5f052681453a991b521c6e6
Eun-Young <48302638+Eun-Young@users.noreply.github.com>
2021-07-14 20:56:34 +0900

    eunyoung init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Appbar from "./components/ex01/Appbar";
+import MainList from "./components/ex01/MainList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Appbar></Appbar>
+      <MainList></MainList>
+      {/*<Grid></Grid>*/}
     </div>
   );
 }
